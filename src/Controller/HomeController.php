c1749118390d1ce12249afda54d015b3ae50eb84
hugal <hvinchon@gmail.com>
2015-07-08 10:20:23 +0200

    removed doctrine and reverted to pdo

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,60 +10,42 @@ class HomeController{
     private $request;
 	private $view;
     private $connection;
-    private $entityManager;
+
 
     /**
      * @param $request Request
      * @param $view \Twig_Environment
      * @param $connection \PDO
      */
-	public function __construct($request, $view, $entityManager){
-		$this->request = $request;
+    public function __construct($request, $view, $connection){
+        $this->request = $request;
         $this->view = $view;
-        $this->entityManager = $entityManager;
-	}
+        $this->connection = $connection;
+    }
 
-	public function indexAction(){
+    public function indexAction(){
 		return $this->view->render("index.twig",
             ["product_thumbnails" => $this->getProducts(5),
             "carousel_products" => $this->getProducts(3)]);
 	}
-//
-//    private function getProducts($number)
-//    {
-//
-//        $sql = "SELECT * FROM `product` LIMIT 0,:number";
-//
-//        $requete = $this->connection->prepare($sql);
-//        // $requete->bindValue(":number", $number);
-//        $requete->bindValue(':number',  $number, \PDO::PARAM_INT);
-//        $requete->execute();
-//        $products = $requete->fetchAll(\PDO::FETCH_CLASS, "TroisWA\\Shop\\Model\\Product");
-//
-//        return $products;
-//
-//
-//    }
 
-    private function getProducts($number) {
-//        $productRepository = $this->entityManager->getRepository("TroisWA\\Shop\\Model\\Product");
+    private function getProducts($number)
+    {
 
-//        return $productRepository->findAll();
+        $sql = "SELECT * FROM `product` LIMIT 0,:number";
 
-//       return $productRepository ->findBy(
-//            array('id' => 0),        // $where
-//            array('id' => 'ASC'),    // $orderBy
-//            4,                        // $limit
-//            2                          // $offset
-//        );
+        $requete = $this->connection->prepare($sql);
+        // $requete->bindValue(":number", $number);
+        $requete->bindValue(':number',  $number, \PDO::PARAM_INT);
+        $requete->execute();
+        $products = $requete->fetchAll(\PDO::FETCH_CLASS, "TroisWA\\Shop\\Model\\Product");
 
-        return $productRepository = $this->entityManager
-            ->createQuery("SELECT p FROM TroisWA\\Shop\\Model\\Product p")
-            ->setMaxResults($number)
-            ->execute();
+        return $products;
 
 
     }
+
+
 
 
 
--- www/index.php
+++ www/index.php
@@ -1,7 +1,7 @@
 <?php 
 
 require_once __DIR__ . "/../vendor/autoload.php";
-require_once __DIR__ . "/../db/doctrine.php";
+
 
 $loader = new Twig_Loader_Filesystem(__DIR__."/../view");
 $twig = new Twig_Environment($loader, 
@@ -28,7 +28,7 @@ use Symfony\Component\HttpFoundation\Request;
 $request = Request::createFromGlobals();
 
 
-$controller = new \TroisWA\Shop\Controller\HomeController($request, $twig, $entityManager);
+$controller = new \TroisWA\Shop\Controller\HomeController($request, $twig, $connection);
 
 echo $controller->indexAction();
 
